fe57fa21d0ff5674b06e60cb989e1c38d7054f91
jmausolf <jgm346@gmail.com>
2015-07-10 11:22:21 -0500

    Adding filter to remove departments CSVs.

---
+++ remove_depts.sh
@@ -0,0 +1,21 @@
+##################################
+###                            ###
+###      Joshua G. Mausolf     ###
+###    Computation Institute   ###
+###    University of Chicago   ###
+###                            ###
+##################################
+
+### This script exists to assist in the removal of filtered CSVs created by
+### department_filter.py. 
+
+### Because this filter runs using an append method, simply re-running the
+### script does not overwrite past results. To evaluate changes, the original
+### files must first be removed.
+
+### This script simplifies that process.
+
+
+rm ../__DEPT*
+
+
